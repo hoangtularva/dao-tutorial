5fd76c1caa77c9e5f049c70e94186460d688e451
Tran Hoang Tu <50492205+hoangtularva@users.noreply.github.com>
2023-02-14 23:06:07 +0700

    Build a DAO for your NFT

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xA5991A7f5753F5Ff9b100fDfb29a0D72A4540536";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
